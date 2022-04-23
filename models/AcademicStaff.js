e427468aee26abc11e2cd879cfe8ea7957f8933f
yasith <it19966922@my.sliit.lk>
2022-04-23 18:56:04 +0530

    create academic staff schema

---
+++ models/AcademicStaff.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const academicStaffSchema = new Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    academicStaffId:{
+        type:String,
+        trim:true,
+        required: true,
+        unique: true
+    },
+    type:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        trim:true,
+        min:6,
+        max:64
+    },
+    faculty:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    contactNumber:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    image:{
+        url : String,
+        public_id : String
+    },
+
+},{timestamps:true});
+
+export default mongoose.model('AcademicStaff',academicStaffSchema);
