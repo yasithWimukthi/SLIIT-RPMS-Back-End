cea4d8bb5624f6157e1792e05114b610b885d02b
yasith <it19966922@my.sliit.lk>
2022-06-03 23:09:44 +0530

    add files

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 import express from "express";
 import bodyParser from "body-parser";
-import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
-dotenv.config();
 
 import authRoutes from "./routes/auth.js";
-const app = express();
+import researchGroupRoutes from "./routes/ResearchGropRoutes.js";
+import studentRoutes from "./routes/StudentRoutes.js";
+import adminRoutes from "./routes/AdminRoutes.js";
+import acedemicStaffRoutes from "./routes/AcademicStaffRoutes.js";
+import chatRoutes from './routes/ChatRoutes.js';
 
+dotenv.config();
+
+const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(morgan('dev'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     next();
 });
 
 
-app.use('/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/researchGroup", researchGroupRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/acedemicStaff", acedemicStaffRoutes);
+app.use("/api/chat", chatRoutes);
+
 
 app.use((error, req, res, next) => {
     console.log(error);
@@ -34,14 +47,12 @@ app.use((error, req, res, next) => {
     res.status(status).json({ message: message, data: data });
 });
 
+app.listen(process.env.PORT || 8000);
 mongoose
-    .connect(
-        process.env.CONNECTION_URL
-    )
+    .connect(process.env.CONNECTION_URL)
     .then(() => {
-        app.listen(process.env.PORT || 8000);
-        console.log('Connected to database');
+        console.log("Connected to database");
     })
-    .catch(err => {
+    .catch((err) => {
         console.log(err);
     });
