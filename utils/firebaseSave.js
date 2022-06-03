2905bb22752f1b389c16f48836869baa0a041b3f
MadhaviImashi <madhaviimashi@gmail.com>
2022-06-04 01:25:57 +0530

    firebase configs

--- utils/firebaseSave.js
+++ utils/firebaseSave.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { uploadBytes } from "firebase/storage";
 
-import fs from 'fs';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDN-4pKXq2Gq_41FPrVcK7-h9dejDo2Hbg",
-  authDomain: "student-management-f2e04.firebaseapp.com",
-  projectId: "student-management-f2e04",
-  storageBucket: "student-management-f2e04.appspot.com",
-  messagingSenderId: "122381542908",
-  appId: "1:122381542908:web:7d7ef4be9954904734be79",
-};
+    apiKey: "AIzaSyACwQjIecE6LAmhgcamXjZ06vU15esnZ2Q",
+    authDomain: "af-rpms.firebaseapp.com",
+    projectId: "af-rpms",
+    storageBucket: "af-rpms.appspot.com",
+    messagingSenderId: "532242515311",
+    appId: "1:532242515311:web:9b13fdd15dfd671b3a149a",
+    measurementId: "G-41L16215K7"
+  };
 
 const app = initializeApp(firebaseConfig);
 
@@ -21,7 +20,7 @@ const storage = getStorage(app);
 
 export default (file,cb)=>{
     const storageRef = ref(storage, file.originalname);
-    uploadBytes(storageRef, file.buffer).then((snapshot) => {
+    uploadBytes(storageRef, file.buffer).then(() => {
        return getDownloadURL(ref(storage, file.originalname));
     }).then(url=>{
         cb(null,url);
